4ab16c3aec93f5e1b1212a068dcec28f10e0960c
Abdul Razak <sendmesymbols@gmail.com>
2020-10-16 16:56:13 +0500

    deploy 5

--- polygon/settings.py
+++ polygon/settings.py
@@ -48,7 +48,7 @@ MANAGERS = ADMINS
 
 _DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1"
 
-ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
+ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS", "localhost,127.0.0.1,zizopixels11.herokuapp.com,zizopixels11-admin.herokuapp.com,zizopixels11-core.herokuapp.com")
 if not ALLOWED_CLIENT_HOSTS:
     if DEBUG:
         ALLOWED_CLIENT_HOSTS = _DEFAULT_CLIENT_HOSTS
@@ -400,7 +400,7 @@ TEST_RUNNER = "polygon.tests.runner.PytestTestRunner"
 
 PLAYGROUND_ENABLED = get_bool_from_env("PLAYGROUND_ENABLED", True)
 
-ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,zizopixels11.herokuapp.com,zizopixels11-core.herokuapp.com"))
+ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,zizopixels11.herokuapp.com,zizopixels11-admin.herokuapp.com,zizopixels11-core.herokuapp.com"))
 ALLOWED_GRAPHQL_ORIGINS = get_list(os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*"))
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
